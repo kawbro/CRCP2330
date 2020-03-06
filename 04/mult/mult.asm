3908c07b732b257324f5847b58d5987a3071ad1f
kawbro <kwicks@smu.edu>
2020-03-06 10:53:41 -0600

    Initial test of Mult and Machine Code. Unclear on how to store R0. Current plan: Add R0 R1 times and store the sum at R2

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,20 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M = 1	//i = 1
+@num
+M = R0	//num = initial R0 value
+ADD R2, R0, num
+(LOOP)
+@i
+D = M
+D = D - R1
+@END
+D; JGT	//If (i - R1) > 0 goto END
+R2 = R1 + num
+@i
+M = M + 1 //i = i + 1
+@LOOP
+(END)
+0; JMP
