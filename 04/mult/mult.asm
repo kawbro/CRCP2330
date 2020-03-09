b0d7dc91583b6e76dde75d45c2f9c491b245dcc6
kawbro <kwicks@smu.edu>
2020-03-09 11:07:33 -0500

    Rewriting Mult.asm because of problems in Mult.hack. Still confused by assembly language. Mult.hack adds incorrectly (according to the CPU emulator)

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,27 +7,15 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-@i
-M = 1	//i = 1
 @R2
-D = 0	//Value of R2 is 0
+M = 0	//Value of R2 is 0
 
 (LOOP)
-@i
-D = M 	//D = i
-@R1
-D = D - A	//D = i, A = @R1; D = i - @R1
-@END
-D; JGT	//If (i - @R1) > 0 goto END
 @R0
-A = M		//A = @R0
+D = M //Puts value of R0 into D
 @R2
-D = A + D 	//@R2 = R0 + R2
-@i
-M = M + 1	//i = i + 1
+M = D + M //Adds R0 to R2
+@R1
+M = M - 1 //Subtracts 1 from R1
 @LOOP
-0; JMP	//Goto LOOP
-
-(END)
-@END
-0; JMP	//Inf. Loop
+M; JGT
