a0c6b22d05d16d942957e2339f6528ad390a692c
kawbro <kwicks@smu.edu>
2020-03-11 05:38:19 -0500

    Fill finished. Forgot to commit. Struggled with jump positionings, but concept was decently easy to grasp.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(RESTART)
+@SCREEN
+D = A
+@0
+M = D	//PUT SCREEN START LOCATION IN RAM0
+
+
+(KBDCHECK)
+
+@KBD
+D = M
+@BLACK
+D;JGT	//Jump if key pressed
+@WHITE
+D;JEQ	//Jump to white
+
+@KBDCHECK
+0;JMP
+
+(BLACK)
+@1
+M = -1	//Fill screen with black
+@CHANGE
+0;JMP
+
+(WHITE)
+@1
+M = 0	//Fill screen with white
+@CHANGE
+0;JMP
+
+(CHANGE)
+@1	//Check screen
+D = M	//D = Black or White
+
+@0
+A = M	
+M = D	//FILL pixel
+
+@0
+D = M + 1	//Next pixel
+@KBD
+D = A - D	//KBD - SCREEN = A
+
+@0
+M = M + 1	//Next pixel
+A = M
+
+@CHANGE
+D;JGT	//Exit if screen is black
+
+@RESTART
+0;JMP
